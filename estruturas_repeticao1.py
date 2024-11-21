f1554f65b1b2cbca0e74034cff9673ece5addadb
JoaoVitorTrab <joaovitorgranjatrabalho@gmail.com>
2024-11-21 12:46:41 -0300

    estruturas_repeticao1.py

---
+++ estruturas_repeticao1.py
@@ -0,0 +1,7 @@
+entrada_idade = ""
+
+while str(entrada_idade) != '0':
+    entrada_idade = input('Digite um número qualquel ou 0 para sair')
+    if entrada_idade == 0:
+        break
+print("Numero digitado:",entrada_idade)
